04ffa6f2922d3b942f455a9b11a6b3b5b6b74429
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-16 11:16:56 +0000

    conversion

--- 01_basic/01_conversion.js
+++ 01_basic/01_conversion.js
@@ -1,8 +1,32 @@
 let num="33"
 let var12= Number(num)
 
-console.log(typeof var12);
+// console.log(typeof var12);
 let isloggedin=1
-let boolloggedin=Boolean(islogged)
+let boolloggedin=Boolean(isloggedin)
 
-console.log(typeof boolloggedin);
+// console.log(typeof boolloggedin);
+ 
+
+     ////********operations*************/////////
+
+//    1>  let value=-3
+//      let negvalue = - value
+//      console.log(negvalue);
+
+
+//    2>  console.log(2+2);
+//      console.log(2-2);
+//      console.log(2*2);
+//      console.log(10/5);
+//      console.log(2%2);
+//      console.log(2**3);
+
+//   3>   
+       
+
+
+
+
+
+
