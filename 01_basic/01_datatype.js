00954dd97046b60ad1f881d4b929e848c1a15146
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-16 10:20:33 +0000

    datatypes

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,20 @@
+"use strict";//it uses the code as a newer version of code
+//alert(3+3) this is not a borwser we using gh
+//DATATYPE
+let name="soha"//this is string
+let age=18//number value
+let isloggedin=false //jab user logged in ho tab ye use karte
+
+
+//number=>2 to power 53 tak hi use kar skate agar usse jyada h toh we ave to use
+//bigint=>
+//string=>""
+//boolean =>true/false
+//undefined=> 
+    let name1;
+console.log(typeof name);
+console.log(typeof name1);
+//null=>it is define a var that is empty
+
+console.log(typeof null);
+
