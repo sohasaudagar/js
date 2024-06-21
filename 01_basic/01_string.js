653eabf0a0f438b6370470d98fdfa8baa830399b
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-21 02:28:05 +0000

    string

---
+++ 01_basic/01_string.js
@@ -0,0 +1,9 @@
+ //concatenation of string 
+ console.log("hitesh"+"hello");
+  // string interpolation -->by making placeholder aur vahape u can put var 
+  const name= "soha"
+  const repocount=58
+  
+  console.log(`hello my name is ${name} and my repo count${repocount}`);
+   const gamename= new string('soha1')
+   console.log(gamename[0]);
