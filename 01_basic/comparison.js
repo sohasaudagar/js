f72d940aacabef027c05c8b8de50ee2c1bbf31eb
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-21 02:13:22 +0000

    comparison

---
+++ 01_basic/comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>4);
+// console.log(2<4);
+// console.log(2==4);
+// console.log(2<=4);
+// console.log(2!=4);
+
+/// string and num
+//  console.log("2"<4);
+//  console.log("2"<"4");
+
+
+///null and undefined
+
+
+//  console.log( null<0);
+//  console.log(null>0);
+//  console.log(null==0);
+//  console.log(null<=0);
+
+console.log(undefined <0);
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined<=0);
