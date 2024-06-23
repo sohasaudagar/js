b9cefcf57932b7f4edc45d0dcde988c48f44b01f
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-23 05:15:06 +0000

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,25 @@
+const myarr=[0,1,2,3,4,5]
+
+// console.log(myarr);
+
+// //adding element 
+// myarr.push(9)
+// console.log(myarr);
+// console.log(myarr.pop());
+
+
+// const newarr= myarr.join()//--> it convert the array into a string
+// console.log(newarr);
+
+
+// slicing ,splicing
+
+console.log("A",myarr);
+
+const myn1= myarr.slice(1,3)
+console.log("B",myarr);
+console.log(myn1);
+
+const myn2=myarr.splice(1,3)
+console.log("C",myarr);
+console.log(myn2);
