209c4a4d6af7d322c70402e44d92772bf5f3ba62
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-23 02:30:11 +0000

    maths

--- 01_basic/maths_num.js
+++ 01_basic/maths_num.js
@@ -18,3 +18,13 @@ console.log(Math.ceil(4.2));
 console.log(Math.floor(4.9));
 console.log(Math.random()*10+1);//imp for making dice game-->0-1 ke bich me value produce karta
 console.log(Math.min(4,3,6,8));
+
+//koi koi cases me we have to define min and max value
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+// formula-->Math.random()*(max-min +1) *min
