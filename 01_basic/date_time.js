8a29f70d3e6a45481aaf3fc8c04db9bc29721b88
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-23 04:22:38 +0000

    date

---
+++ 01_basic/date_time.js
@@ -0,0 +1,18 @@
+// let mydate= new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toJSON());
+
+
+// let createddate = new Date(2023,2,2)
+// console.log(createddate.toString());
+
+//for the pool result timing
+
+let mytimestamp= new Date()
+console.log(mytimestamp.toLocaleString());
+console.log((Math.floor(Date.now()/1000));
+
+
+
+
