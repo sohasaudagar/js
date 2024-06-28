71214450afc0f0530f259339fedc5a208ea22ebf
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-28 01:08:50 +0000

    array

---
+++ 02_basic/02array.js
@@ -0,0 +1,9 @@
+marvel_heros=["thor","ironman","spiderman"]
+dc_heros=["superman","flash","batman"]
+///////adding two array
+marvel_heros.push(dc_heros)
+console.log(marvel_heros);
+
+const allheros=marvel_heros.concat(dc_heros)
+console.log(allheros);
+
