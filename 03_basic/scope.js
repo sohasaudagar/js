9e94277e18c625bf7a6611c343e9018ccba292b2
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-06-30 02:17:16 +0000

    scope

---
+++ 03_basic/scope.js
@@ -0,0 +1,15 @@
+var c=300//
+let a=100
+if(true){
+    let a=10
+    const b=20
+    var c=3//-->var block ke bahar bhi print ho jata aur let and const nahi hote
+     console.log("INNER:",a);
+
+     }
+      for(i=0;i<Array.length;i++){
+        const element=
+      }
+    console.log(a);
+    // console.log(b);
+    console.log(c);
