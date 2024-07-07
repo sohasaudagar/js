2c6b7ef35b22dac7dff2d9352451f8f0e0d3fda7
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-07-07 02:14:44 +0000

    switch

---
+++ controlflow/swtich.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+ switch (month) {
+    case 1:
+        console.log("januvary");
+        
+     break;
+        
+    case 2:
+         console.log("februvary");
+            
+        break;
+    case 3:
+         console.log("march");
+            
+       break;
+    case 4:
+         console.log("april");
+            
+            break;
+    default:
+        console.log("default");
+        break;
+ }
+
+//   agar string ho toh
+  const month1 ="march"
+ switch (month1) {
+    case "januvary":
+        console.log("1");
+        break;
+    case "februvary":
+         console.log("2");
+        break;
+    case "march":
+         console.log("3");
+        break;
+    case "april":
+         console.log("4");
+        break;
+       
+    default:
+        console.log("default");
+        break;
+ }
