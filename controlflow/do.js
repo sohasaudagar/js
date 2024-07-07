10bddfed67eae16d8ee9a37e0e2b8de8ab21444e
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-07-07 04:33:42 +0000

    while

---
+++ controlflow/do.js
@@ -0,0 +1,18 @@
+let index=0
+while (index<=10) {
+    console.log(`value of index ${index}`);
+    index=index+2
+}
+
+let myarr=['flash','batman','supetman']
+ let arr=0
+ while (arr<=myarr.length) {
+    console.log(`tha value of arr${myarr[arr]}`);
+    arr=arr+1 
+}
+
+let score =1
+do{
+    console.log(`score is${score}`);
+    score++
+}while (score<=10);
