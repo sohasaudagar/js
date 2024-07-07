ac00a142278805ba7bd7a317fe520e19197eb5ba
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-07-07 02:01:19 +0000

    if

---
+++ controlflow/basic.js
@@ -0,0 +1,44 @@
+///if
+//  const logeedin=true
+// if(2 !=3){
+//  console.log("executed");
+// }
+
+//  const temperature=40
+// if(temperature<50){
+//     console.log("less than 50");
+// }
+//  else{
+//     console.log("temperature is greater than 50");
+//  }
+
+//    const balance=1000
+//     if (balance>500) {
+//         console.log("balace is greater than 500");
+//     }
+//     else if(balance<500){
+//         console.log("balance is less than 500");
+//     }
+//     else{
+//         console.log("balanceis equal to 500");
+//     }
+
+
+const userloggedin=true
+const debitcard=true
+    if (userloggedin && debitcard) {
+        console.log("allow to buy");
+    }
+const loggedinfromgoogle=false
+const loggedinfromemail= true
+  if (loggedinfromemail || loggedinfromgoogle) {
+    console.log("user logged in");
+  }
+
+
+
+
+
+
+
+
