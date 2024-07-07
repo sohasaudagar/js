cb220a346ece5fb0814237b60eea606db65826ba
sohasaudagar <163838652+sohasaudagar@users.noreply.github.com>
2024-07-07 04:49:02 +0000

    forof

---
+++ controlflow/forof.js
@@ -0,0 +1,13 @@
+///jab database se value lete tab ye use karte
+//forof
+const arr=[1,2,3,4,5,6]
+for (const num of arr) {
+   // console.log(num);
+}
+ const greetinf="hello world"
+ for (const greet of greetinf) {
+    // console.log(`each char is${greet}`);
+    
+ }
+
+ //maps
